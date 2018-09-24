9a4babba889627bec564df0e8548ed97fc632632
ssizova <sizova96@mail.ru>
2018-09-25 02:02:27 +0700

    This programs works correctly, but without cmake. And does not take into account capitaized or not letters

--- Task0/word_count.cpp
+++ Task0/word_count.cpp
@@ -2,12 +2,35 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <algorithm>
+
+struct WordDescriptor
+{
+	std::string word;
+	int32_t frequency;
+	double frequency_percentage;
+};
+
 
 bool is_separator(char symbol) {
 	return (!(std::isdigit(symbol) || std::isalpha(symbol)));
 }
 
-std::vector<std::string> Parse_into_words(std::string line) {
+
+void Print_vector_in_csv (std::vector<WordDescriptor> words, std::string output) {
+	std::ofstream output_file (output);
+	int32_t size = words.size();
+	output_file << "sep=," << std::endl;
+	for (int32_t i = 0; i < size; ++i)
+	{
+		output_file << words[i].word << "," << words[i].frequency << ",";
+		output_file.precision (4);
+		output_file << words[i].frequency_percentage << std::endl;
+	}
+}
+
+
+std::vector<std::string> Parse_into_words (std::string line) {
 	int32_t number_of_symbols = line.length();
 	char symbol = 0;
 	std::string word = "";
@@ -22,17 +45,13 @@ std::vector<std::string> Parse_into_words(std::string line) {
 			}
 		}
 		else {
-			words.push_back(word);
+			if (word !="") {
+				words.push_back(word);
+			}
 			word = "";
 		}
 	}
 	return words;
-}
-
-void Print_map (std::map<std::string, int32_t> my_map) {
-	for (std::map<std::string,int32_t>::iterator it = my_map.begin(); it!=my_map.end(); ++it)
-    std::cout << it->first << " => " << it->second << '\n';
-
 }
 
 
@@ -48,9 +67,8 @@ std::vector<std::string> Get_lines (std::string input) {
 
 
 std::map<std::string, int32_t> Words_classification (std::map<std::string, int32_t> frequency, std::vector<std::string> words) {
-	std::map<std::string,int32_t>:: iterator it = frequency.find(words[0]);
 	for (int32_t i = 0; i < words.size(); ++i) {
-		it = frequency.find(words[i]);
+		std::map<std::string,int32_t>:: iterator it = frequency.find(words[i]);
 		if (it != frequency.end()) {
 			it->second +=1; 
 		}
@@ -76,28 +94,44 @@ std::vector<double> Frequency_percentage (std::map <std::string, int32_t> my_map
 	std::vector<double> percentage;
 	int32_t i = 0;
 	for (std::map<std::string,int32_t>::iterator it=my_map.begin(); it!=my_map.end(); ++it) {
-    	percentage[i] = it->second * 100 / number_of_words;
-    	++i;
+    	percentage.push_back(it->second * 100.0 / number_of_words);
 	}
 	return percentage;
 }
 
 
-void Words_finder (std::string input) {
-	std::vector<std::string> lines = Get_lines(input); 
-	std::vector<std::string> words;
-	std::map<std::string, int32_t> frequency;
-	
-	for (int32_t i = 0; i < lines.size(); ++i) {
-		words = Parse_into_words (lines[i]);
-		frequency = Words_classification (frequency, words);
+std::vector<WordDescriptor> Sorting (std::map<std::string, int32_t> words) {
+	std::vector<double> percents = Frequency_percentage (words);
+	std::vector<WordDescriptor> sorted_words;
+	WordDescriptor object;
+	int32_t i = 0;
+	for (std::map<std::string,int32_t>::iterator it = words.begin(); it!=words.end(); ++it) {
+    	object.word = it->first;
+    	object.frequency = it->second;
+    	object.frequency_percentage = percents[i];
+    	++i;
+    	sorted_words.push_back(object);
 	}
-
-	Print_map (frequency);
- 	/* сортируем frequency, Frequency_percentage (sorted_frequency), запись в файл */
-
+	std::sort(sorted_words.begin(), sorted_words.end(), [](WordDescriptor d1, WordDescriptor d2){ return d1.frequency > d2.frequency; });
+	return sorted_words;
 }
 
+
+void Words_finder (std::string input, std::string output) {
+	std::vector<std::string> lines = Get_lines(input); 
+	std::vector<std::string> words_in_vector;
+	std::map<std::string, int32_t> words;
+
+
+	
+	for (int32_t i = 0; i < lines.size(); ++i) {
+		words_in_vector = Parse_into_words (lines[i]);
+		words = Words_classification (words, words_in_vector);
+	}
+	std::vector<WordDescriptor> sorted_words = Sorting(words);
+ 	Print_vector_in_csv (sorted_words,output);
+
+}
 
 int main()
 {
@@ -105,9 +139,7 @@ int main()
 	std::string input = "";
 	std::string output = "";
 	std::cin >> input >> output ;
-	std::ofstream output_file (output);
-	output_file << "wewefw";
-   	Words_finder(input);
+   	Words_finder(input, output);
 
 	return 0;
 }
