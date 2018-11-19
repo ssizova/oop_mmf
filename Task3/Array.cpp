90300695271c9448deb276475d7fb8e11aee75fa
ssizova <sizova96@mail.ru>
2018-11-19 18:35:04 +0700

    Create compile-time integration using Simpson method (three nodes)

---
+++ Task3/Array.cpp
@@ -0,0 +1,21 @@
+//
+// Created by sofia on 19.11.18.
+//
+#include <memory>
+#include <bits/unique_ptr.h>
+#include <strings.h>
+template <class T>
+class Array {
+    std::unique_ptr<T> data;
+public:
+    explicit Array(uint32_t size){
+        data= std::make_unique<T[]>(size);
+    }
+    T Get(uint32_t) {
+        return data.get()[index];
+    }
+    void Set(uint32_t index, T value){
+        data.get()[index] = value;
+    }
+
+};
